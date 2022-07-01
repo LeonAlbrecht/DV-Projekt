e3b9373320de3be598ac496940a5f1c50fdf9bd3
LeonAlbrecht <101879297+LeonAlbrecht@users.noreply.github.com>
2022-07-01 12:15:19 +0200

    präsi

---
+++ SchereSteinPapierProjekt (alt)/src/NormalPvC.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class NormalPvC {
+	
+	JFrame frame = new JFrame();
+	
+	NormalPvC() {
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800,400);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		
+	}
+
+}
